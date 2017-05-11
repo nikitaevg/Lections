6d3a9fbc94260e0a4232dfecff42c026fc01dbba
nikitaevg <vorobyev.nikita@gmail.com>
2017-05-11 19:02:34 +0300

    asm

---
+++ asm/fourth_sem/2017_03_25/b.c
@@ -0,0 +1,4 @@
+http://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-manual-325462.html
+
+См. фото про bad speculation итд
+Показывает в какой ресурс упирается программа
