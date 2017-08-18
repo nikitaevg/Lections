10b5a86893cb0fbcebf89bebdb0a17c9d0d0d6cf
nikitaevg <vorobyev.nikita@gmail.com>
2017-08-18 21:08:28 +0300

    Asm conspects deleted

--- asm/fourth_sem/2017_02_18/ex3.c
+++
@@ -1,6 +0,0 @@
-void _memcpy_r (void const* src, void* dst, int size) {
-
-}
-void _memcpy_d (void const* src, void* dst, int size) {
-
-}
--- asm/fourth_sem/2017_03_25/b.c
+++
@@ -1,4 +0,0 @@
-http://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-manual-325462.html
-
-См. фото про bad speculation итд
-Показывает в какой ресурс упирается программа
